309791436dc2ff61d2243dcbf052bb3858598896
Alok-MBaR <aloksharma1236@gmail.com>
2021-11-18 13:55:15 +0100

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,7 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
            
         }
 
@@ -47,6 +48,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
